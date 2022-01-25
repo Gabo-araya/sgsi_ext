ba0d97c6caaf1bba35bce40d5e2435b62a7e9635
Mauricio Castro <mauricio@magnet.cl>
2022-01-25 18:20:10 -0300

    feat: install docker with sudo, manage it as non-root

--- scripts/docker-install.sh
+++ scripts/docker-install.sh
@@ -3,21 +3,32 @@ set -e
 source scripts/utils.sh
 
 title_print "Installing Docker..."
+# Check if docker is already installed
 if [[ $(which docker) && $(docker --version) ]]; then
   color_print $green "Skipped! $(docker --version) found."
 else
+  # Download and install
   curl -fsSL https://get.docker.com -o get-docker.sh
   chmod +x get-docker.sh
-  ./get-docker.sh
+  sudo ./get-docker.sh
   rm get-docker.sh
-  color_print $green "Docker installation completed"
+
+  # Allow us to manage docker as a non-root
+  sudo groupadd docker || true
+  sudo usermod -aG docker $USER
+  newgrp docker
+
+  color_print $green "Docker installation completed."
 fi
 
 title_print "Installing Docker-compose..."
+# Check if docker-compose is already installed
 if [[ $(which docker-compose) && $(docker-compose --version) ]]; then
   color_print $green "Skipped! $(docker-compose --version) found."
 else
+  # Download and allow execute
   sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
-  color_print $green "Docker-compose installation completed"
+
+  color_print $green "Docker-compose installation completed."
 fi
