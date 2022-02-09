5b33d81db326fc4b6f22bb133568a0e481a9eb5a
Tomás Andrighetti <tomas.andrighetti@magnet.cl>
2022-02-09 12:06:48 -0300

    chore: log current startup operation

--- docker/prod_entrypoint.sh
+++ docker/prod_entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 set -e
+source scripts/utils.sh
+
+title_print "Migrating..."
 poetry run ./manage.py migrate
+
+title_print "Running gunicorn..."
 poetry run gunicorn project.wsgi:application --config docker/gunicorn_conf.py
