6ee61ee8ef19f8a130c9c2b5aaee5de0f3fa2696
Tomás Andrighetti <tomas.andrighetti@magnet.cl>
2022-02-21 15:47:59 -0300

    fix: group listing (again)

--- scripts/docker-install.sh
+++ scripts/docker-install.sh
@@ -17,7 +17,7 @@ else
 fi
 
 # Check if we are allowed to manage docker as a non-root
-if ! id -ng "$USER" | grep -qw docker; then
+if ! groups | grep -qw docker; then
   sudo groupadd docker &>/dev/null || true
   sudo usermod -aG docker "$USER"
   echo "Please reboot your computer to use Docker without sudo." >> quickstart-messages.log
