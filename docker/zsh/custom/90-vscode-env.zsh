9bf8761733e694db33a2a86e5dc9bcc45dcdfd36
Tomás Andrighetti <tomas.andrighetti@magnet.cl>
2022-03-08 20:21:31 -0300

    fix: prevent overwriting PATH

--- docker/zsh/custom/90-vscode-env.zsh
+++ docker/zsh/custom/90-vscode-env.zsh
@@ -9,15 +9,21 @@
 # (I think it's copied only when vscode is first reopened)
 # Idea: commit unhashed known_hosts to repo, and bind-mount them to /etc/ssh/ssh_known_hosts
 
-if [[ -z "$REMOTE_CONTAINERS" ]]; then
-  if [[ -r /dev/shm/env_from_vscode_terminal ]]; then
-    while read line; do   # https://stackoverflow.com/questions/15235729/sourcing-env-output/15248663#15248663
-      export "$line"
-    done < /dev/shm/env_from_vscode_terminal
+function () {   # Scope  (https://stackoverflow.com/questions/13667284/how-do-i-keep-functions-variables-local-to-my-zshrc/13670749#13670749)
+  if [[ -z "$REMOTE_CONTAINERS" ]]; then
+    if [[ -r /dev/shm/env_from_vscode_terminal ]]; then
 
-    # Note: this will define REMOTE_CONTAINERS from now on (should be denylisted?)
+      local ignored_vars=(PATH REMOTE_CONTAINERS)
 
-  else
-    echo $fg[yellow]vscode env not loaded$reset_color
+      while read line; do   # Correctly handes unquoted  https://stackoverflow.com/questions/15235729/sourcing-env-output/15248663#15248663
+        local name=$(echo "$line" | sed 's/=.*//')
+        if (( $ignored_vars[(Ie)$name] == 0 )); then  # "if $name not in $ignored_vars"  https://unix.stackexchange.com/questions/411304/how-do-i-check-whether-a-zsh-array-contains-a-given-value/411331#411331
+          export "$line"
+        fi
+      done < /dev/shm/env_from_vscode_terminal
+
+    else
+      echo $fg[yellow]vscode env not loaded$reset_color
+    fi
   fi
-fi
+}
