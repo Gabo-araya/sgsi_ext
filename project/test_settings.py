a53f61fb8e7bfff8f51fe2e87f23af30b336d52a
Nicolás Stuardo Díaz <nicolas.stuardo@magnet.cl>
2022-03-08 18:35:49 -0300

    chore: use a separate config for test runs

---
+++ project/test_settings.py
@@ -0,0 +1,12 @@
+"""
+Settings overrides for unit testing.
+"""
+
+from .settings import *  # noqa:F401,F403
+
+# Use a simpler password hash algorithm to speed up test execution when password hashing
+# is intensively used, directly or indirectly.
+# (See https://docs.djangoproject.com/en/4.0/topics/testing/overview/#password-hashing)
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.UnsaltedMD5PasswordHasher',
+]
