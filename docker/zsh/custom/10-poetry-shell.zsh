b3303437cc62d942a9eda73499f997624d04bb77
Tomás Andrighetti <tomas.andrighetti@magnet.cl>
2022-03-08 15:25:45 -0300

    chore: prefer own venv source

--- docker/zsh/custom/10-poetry-shell.zsh
+++ docker/zsh/custom/10-poetry-shell.zsh
@@ -1,8 +1,3 @@
 export VIRTUAL_ENV_DISABLE_PROMPT=x
 
-if [[ -n "$REMOTE_CONTAINERS" ]]; then
-  : # Remote-Containers will source the virtualenv later.
-    # An alternative would be to set "python.terminal.activateEnvironment": false.
-else
-  source "$(cd /usr/src/app && poetry env info --path)/bin/activate"
-fi
+source "$(cd /usr/src/app && poetry env info --path)/bin/activate"
