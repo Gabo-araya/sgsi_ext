8d58c8c5a590ba89754dc13077b0229459a65034
Tomás Andrighetti <tomas.andrighetti@magnet.cl>
2022-03-08 12:11:58 -0300

    style: add source alternative

--- docker/zsh/custom/10-poetry-shell.zsh
+++ docker/zsh/custom/10-poetry-shell.zsh
@@ -1,7 +1,8 @@
 export VIRTUAL_ENV_DISABLE_PROMPT=x
 
 if [[ -n "$REMOTE_CONTAINERS" ]]; then
-  : # Remote-Containers will source the virtualenv later
+  : # Remote-Containers will source the virtualenv later.
+    # An alternative would be to set "python.terminal.activateEnvironment": false.
 else
   source "$(cd /usr/src/app && poetry env info --path)/bin/activate"
 fi
