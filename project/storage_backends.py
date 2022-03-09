3b8e4d39677aae5851c8f24cf37315f652e9ff70
Tomás Andrighetti <tomas.andrighetti@magnet.cl>
2022-03-09 17:46:07 -0300

    fix: import

--- project/storage_backends.py
+++ project/storage_backends.py
@@ -1,12 +1,12 @@
-import storages
+from storages.backends import s3boto3
 
 
-class S3StaticStorage(storages.backends.s3boto3.S3StaticStorage):
+class S3StaticStorage(s3boto3.S3StaticStorage):
     location = "static"
     default_acl = "public-read"
 
 
-class S3MediaStorage(storages.backends.s3boto3.S3Boto3Storage):
+class S3MediaStorage(s3boto3.S3Boto3Storage):
     location = "media"
     default_acl = "private"
     file_overwrite = False
