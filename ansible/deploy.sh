b8cb5dbbb5994d73970b5099383a9169df8c4ab3
Tomás Andrighetti <tomas.andrighetti@magnet.cl>
2022-03-24 14:38:25 -0300

    chore: clone before prompting for .env

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -28,6 +28,12 @@ fi
 limit=$1
 # improvement: implement update for two servers at same time
 
+
+ansible-role() {
+  ansible "$limit" --playbook-dir playbooks/ -m include_role -a name="$1"
+}
+
+
 if [[ "$(basename "$0")" == "update.sh" ]]; then
   tags="--tags update"
 else
@@ -35,19 +41,33 @@ else
 
   echo "Checking for .env existance on $limit..."
 
-  stat_ansible_res=0
-  stat_json=$(ANSIBLE_STDOUT_CALLBACK=json ansible-playbook -l "$limit" playbooks/stat_dotenv.yml) || stat_ansible_res=$?
-  if (( stat_ansible_res > 0 )); then
+  stat_ansible_rc=0
+  stat_json=$(ANSIBLE_STDOUT_CALLBACK=json ansible-playbook -l "$limit" playbooks/check-cloned.yml) || stat_ansible_rc=$?
+  if (( stat_ansible_rc > 0 )); then
     echo "$stat_json" | jq
-    exit $stat_ansible_res
+    exit $stat_ansible_rc
   fi
 
-  stat_jq_res=0
-  echo "$stat_json" | jq --exit-status '.plays[0].tasks[0].hosts[].stat.exists' >/dev/null || stat_jq_res=$?
-  if (( stat_jq_res > 1 )); then
-    exit $stat_jq_res
+  cloned_jq_rc=0
+  echo "$stat_json" | jq --exit-status '.plays[0].tasks[0].hosts[].stat.exists' >/dev/null || cloned_jq_rc=$?
+  if (( cloned_jq_rc > 1 )); then
+    exit $cloned_jq_rc
+  fi
 
-  elif (( stat_jq_res == 1 )); then
+  env_jq_rc=0
+  echo "$stat_json" | jq --exit-status '.plays[0].tasks[1].hosts[].stat.exists' >/dev/null || env_jq_rc=$?
+  if (( env_jq_rc > 1 )); then
+    exit $env_jq_rc
+  fi
+
+  if (( cloned_jq_rc == 1 )); then
+    # Run git clone before asking .env questions, so they don't get lost.
+    # It would have been better to mkdir in server and copy .env, and then clone,
+    # but git fails with "not an empty directory".
+    ansible-role git-clonepull
+  fi
+
+  if (( env_jq_rc == 1 )); then
     # Prompt for new .env file
     cd ..
     (umask 177; scripts/env-init-prod.sh "$limit")
