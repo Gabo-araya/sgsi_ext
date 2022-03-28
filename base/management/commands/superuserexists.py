c17ed30df5d61470a0922590ee78e354503c9615
Tomás Andrighetti <tomas.andrighetti@magnet.cl>
2022-03-28 18:43:07 -0300

    refactor: query for existance only

--- base/management/commands/superuserexists.py
+++ base/management/commands/superuserexists.py
@@ -8,5 +8,5 @@ class Command(BaseCommand):
     help = "Returns 0 only if there's an active superuser account"
 
     def handle(self, *args, **options):
-        exists = User.objects.filter(is_superuser=True, is_active=True)
-        sys.exit(0 if exists else 1)
+        superusers = User.objects.filter(is_superuser=True, is_active=True)
+        sys.exit(0 if superusers.exists() else 1)
