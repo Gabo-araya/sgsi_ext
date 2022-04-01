f29eeeab915c6e9963d6474e11d946a0fdcf35a6
Tomás Andrighetti <tomas.andrighetti@magnet.cl>
2022-04-01 20:24:10 -0300

    fix: allow http in development

--- project/settings.py
+++ project/settings.py
@@ -303,7 +303,7 @@ SECURE_HSTS_SECONDS = 31536000
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
 
-SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = not DEBUG
 # Requests are redirected by nginx, but setting this here silences a warning.
 
 SESSION_COOKIE_SECURE = True
