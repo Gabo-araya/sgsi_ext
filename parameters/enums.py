95dafb23b266a183f43ac0673ecfb56841c5c135
Nicolás Stuardo Díaz <nicolas.stuardo@magnet.cl>
2022-04-04 11:08:27 -0400

    refactor: rewrite parameters kinds to use Choices API

--- parameters/enums.py
+++ parameters/enums.py
@@ -1,7 +1,14 @@
-# standard library
-
 # django
+from django.db.models.enums import TextChoices
 from django.utils.translation import ugettext_lazy as _
 
-# validators
 
+class ParameterKind(TextChoices):
+    """Represents the available choices of parameter kinds"""
+
+    INT = ("int", _("integer"))
+    STR = ("str", _("text"))
+    TIME = ("time", _("time"))
+    DATE = ("date", _("date"))
+    JSON = ("json", _("json"))
+    BOOL = ("bool", _("boolean"))  # 'true', '1' or 'yes'
--- parameters/models.py
+++ parameters/models.py
@@ -18,7 +18,7 @@ from base.models import BaseModel
 from parameters.definitions import ParameterDefinitionList
 
 # enums
-from parameters.enums import ParameterDefinitionList
+from parameters.enums import ParameterKind
 
 
 class Parameter(BaseModel):
@@ -35,14 +35,7 @@ class Parameter(BaseModel):
         max_length=255,
         verbose_name=_("kind"),
         editable=False,
-        choices=(
-            ("int", _("integer")),
-            ("str", _("text")),
-            ("time", _("time")),
-            ("date", _("date")),
-            ("json", _("json")),
-            ("bool", _("boolean")),  # 'true', '1' or 'yes'
-        ),
+        choices=ParameterKind.choices,
     )
     cache_seconds = models.PositiveIntegerField(
         verbose_name=_("cache seconds"),
@@ -50,25 +43,25 @@ class Parameter(BaseModel):
     )
 
     def clean(self):
-        if self.kind == "int":
+        if self.kind == ParameterKind.INT:
             try:
                 int(self.raw_value)
             except ValueError:
                 raise ValidationError(_("Invalid input"))
 
-        if self.kind == "time":
+        if self.kind == ParameterKind.TIME:
             try:
                 time.strptime(self.raw_value, "%H:%M")
             except ValueError:
                 raise ValidationError(_("Invalid time format"))
 
-        if self.kind == "date":
+        if self.kind == ParameterKind.DATE:
             try:
                 datetime.datetime.strptime(self.raw_value, "%Y-%m-%d")
             except ValueError:
                 raise ValidationError(_("Invalid time format"))
 
-        if self.kind == "json":
+        if self.kind == ParameterKind.JSON:
             try:
                 json.loads(self.raw_value)
             except json.JSONDecodeError:
@@ -88,19 +81,19 @@ class Parameter(BaseModel):
 
     @classmethod
     def process_value(cls, kind, raw_value):
-        if kind == "int":
+        if kind == ParameterKind.INT:
             return int(raw_value)
 
-        if kind == "json":
+        if kind == ParameterKind.JSON:
             return json.loads(raw_value)
 
-        if kind == "time":
+        if kind == ParameterKind.TIME:
             return time.strptime(raw_value, "%H:%M")
 
-        if kind == "date":
+        if kind == ParameterKind.DATE:
             return datetime.datetime.strptime(raw_value, "%Y-%m-%d")
 
-        if kind == "bool":
+        if kind == ParameterKind.BOOL:
             return raw_value.lower() in ("yes", "true", "1")
 
         return raw_value
