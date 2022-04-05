91a7535e8e42f256b1cb794ce695f8c7788cf450
Tomás Andrighetti <tomas.andrighetti@magnet.cl>
2022-04-05 12:39:13 -0400

    docs: add vscode instructions at the end of quickstart

--- quickstart.sh
+++ quickstart.sh
@@ -72,8 +72,17 @@ if [[ $input_lower == y ]]; then
 fi
 
 # Done
-color_print $green 'Completed! http://localhost:8000'
+color_print $green 'Completed!'
+
 if [ -f quickstart-messages.log ]; then
   color_print $yellow "$(cat quickstart-messages.log)"
   rm quickstart-messages.log
 fi
+
+color_print $green "After rebooting if required,
+open this folder in VSCode,
+install the recommended 'Remote - Containers' extension if prompted,
+and click 'Reopen in Container' when prompted. (Or press F1 and type 'Reopen in Container')
+
+Then in a VSCode terminal (if a black-and-white one appears (bash), press ctrl-D and open a new one),
+run 'dj runserver' and access the site at http://localhost:8000"
