ac7f7d8e4d01d2fb459de13e5b036222498dbe05
Joao S. López <joao@magnet.cl>
2022-04-26 18:08:55 -0400

    chore: set default auto field on initial migrations

--- regions/migrations/0001_initial_with_fixtures.py
+++ regions/migrations/0001_initial_with_fixtures.py
@@ -52,7 +52,7 @@ class Migration(migrations.Migration):
             fields=[
                 (
                     "id",
-                    models.AutoField(
+                    models.BigAutoField(
                         auto_created=True,
                         primary_key=True,
                         serialize=False,
@@ -110,7 +110,7 @@ class Migration(migrations.Migration):
             fields=[
                 (
                     "id",
-                    models.AutoField(
+                    models.BigAutoField(
                         auto_created=True,
                         primary_key=True,
                         serialize=False,
