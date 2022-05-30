d0457ccea56a4fce04047985c026bf76e03363b8
Nicolás Stuardo Díaz <nicolas.stuardo@magnet.cl>
2022-05-30 18:32:07 -0400

    perf: return empty when settings.CRITICAL_ENVIRONMENT == True

--- base/templatetags/environment_badge.py
+++ base/templatetags/environment_badge.py
@@ -1,16 +1,24 @@
 # django
 from django import template
 from django.conf import settings
+from django.template.loader import get_template
 
 register = template.Library()
 
 
-@register.inclusion_tag("includes/environment_badge.pug")
+def return_empty_context():
+    return ""
+
+
 def env_badge():
     """Displays a tag depending on the environment the app is running on."""
-    if settings.CRITICAL_ENVIRONMENT:
-        return {"app_environment": "production"}
-    elif settings.DEBUG:
+    if settings.DEBUG:
         return {"app_environment": "debug"}
     else:
         return {"app_environment": "staging"}
+
+
+if not settings.CRITICAL_ENVIRONMENT:
+    register.inclusion_tag(get_template("includes/environment_badge.pug"))(env_badge)
+else:
+    register.simple_tag(return_empty_context, name="env_badge")
