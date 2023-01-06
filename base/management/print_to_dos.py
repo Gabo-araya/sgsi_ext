13341fec8e5c5b3c9e7f51180569de0b00ba5fc7
Nicolás Stuardo Díaz <nicolas.stuardo@magnet.cl>
2023-01-06 13:12:23 -0300

    fix: specify files location for ripgrep execution
    In some environments, ripgrep is run without a stdin, causing it to
    wait indefinitely for data coming from it. Passing a "." fixes it.

--- base/management/print_to_dos.py
+++ base/management/print_to_dos.py
@@ -11,10 +11,12 @@ def print_to_dos():
             "--pretty",
         )
         pattern = "T" + "ODO|FIXM" + "E"  # Prevent searching itself
-        subprocess.run((*common_args, "--ignore-file=project/.todoignore", pattern))
-        subprocess.run((*common_args, "--glob=*.env*", pattern))
+        subprocess.run(
+            (*common_args, "--ignore-file=project/.todoignore", pattern, ".")
+        )
+        subprocess.run((*common_args, "--glob=*.env*", pattern, "."))
         # Run twice because of unoverridable precedences
-        # https://github.com/BurntSushi/ripgrep/issues/1734#issuecomment-730769439
+        # (See https://github.com/BurntSushi/ripgrep/issues/1734#issuecomment-730769439)
     except Exception:
         # Continue instead of breaking runserver
         print(traceback.format_exc())
