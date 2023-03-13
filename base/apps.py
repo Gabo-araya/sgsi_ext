0ce180861de901f9dcb57f150e961df4ca88813e
Nicolás Stuardo Díaz <nicolas.stuardo@magnet.cl>
2023-03-13 11:38:55 -0300

    fix: move import to avoid AppRegistryNotReady

--- base/apps.py
+++ base/apps.py
@@ -2,8 +2,6 @@ from django.apps import AppConfig
 from django.db.models.signals import post_delete
 from django.db.models.signals import post_save
 
-from base.utils import get_subclasses
-
 
 class BaseConfig(AppConfig):
     name = "base"
@@ -12,6 +10,7 @@ class BaseConfig(AppConfig):
         # Register post_save and post_delete handlers
         from base import signals
         from base.models import BaseModel
+        from base.utils import get_subclasses
 
         for subclass in get_subclasses(BaseModel):
             if subclass._meta.abstract or subclass._meta.proxy:
