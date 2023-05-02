d1b16c1760473d218253efdf36228eafc6d78985
Nicolás Stuardo Díaz <nicolas.stuardo@magnet.cl>
2023-05-01 22:08:51 -0400

    chore: use perl to do variable substitution
    Unlike sed, which is available on BSD and GNU versions, perl has an uniform implementation on BSD and GNU systems.

--- quickstart.sh
+++ quickstart.sh
@@ -53,7 +53,7 @@ echo "docker-compose build && docker-compose down" | newgrp docker
 env_file='.env'
 # VIRTUAL_ENV must be unset for poetry to generate the path itself
 virtual_env=$(echo "docker-compose run --rm -T django env --unset=VIRTUAL_ENV poetry env info --path" | newgrp docker)
-sed -i "s|{{virtual_env}}|$virtual_env|g" $env_file
+perl -pi -e "s|{{virtual_env}}|$virtual_env|g" $env_file
 
 # Set vscode to use python in poetry env
 mkdir -p .vscode
