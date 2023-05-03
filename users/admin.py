7a9bbd07f7f5edd9538c7ea08c2ed1b6703167d8
Nicolás Stuardo Díaz <nicolas.stuardo@magnet.cl>
2023-05-03 10:54:47 -0400

    feat: add force_logout action to UserAdmin, log when users are logged out

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,10 @@
 """ Admin page configuration for the users app """
-
+import logging
 
 from django.contrib import admin
 from django.contrib import messages
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.contrib.sessions.models import Session
 from django.utils.html import format_html
 from django.utils.translation import gettext_lazy as _
 
@@ -12,21 +13,35 @@ from users.forms import UserChangeForm
 from users.forms import UserCreationForm
 from users.models import User
 
+logger = logging.getLogger(__name__)
 
+
+@admin.action(description=_("Log out from all devices"))
 def force_logout(modeladmin, request, queryset):
-    for user in queryset:
-        user.force_logout()
+    user_id = request.user.pk
+    requested_user_ids = set(queryset.values_list("pk", flat=True))
+    logged_out_count = 0
 
-    # TODO add log to register this action
-
-    messages.add_message(
-        request,
-        messages.SUCCESS,
-        _("Selected users where logged out"),
+    logger.info(
+        "User #{:d} has requested forced logout of user(s): {:s}.".format(
+            user_id,
+            ", ".join(str(uid) for uid in requested_user_ids),
+        ),
     )
 
+    for s in Session.objects.all():
+        user_id = int(s.get_decoded().get("_auth_user_id"))
+        if user_id in requested_user_ids:
+            logger.info(f"Logged out user #{user_id}")
+            s.delete()
+            logged_out_count += 1
 
-force_logout.short_description = _("Logs out the user from all devices")
+    if logged_out_count > 0:
+        info_msg = _(f"{logged_out_count:d} user(s) where logged out.")
+    else:
+        info_msg = _("No active sessions were found for the selected users.")
+
+    messages.add_message(request, messages.SUCCESS, info_msg)
 
 
 class UserAdmin(DjangoUserAdmin):
@@ -34,6 +49,7 @@ class UserAdmin(DjangoUserAdmin):
 
     add_form_template = "admin/users/user/add_form.html"
     change_form_template = "loginas/change_form.html"
+    actions = [force_logout]
 
     add_form = UserCreationForm
     list_display = (
