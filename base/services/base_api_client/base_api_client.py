0330dacd99f4882c195b4eaa1b3475a973cfb754
Raul Esteban <raul.esteban@magnet.cl>
2023-05-16 17:16:16 -0400

    feat: update base api client to use logs

--- base/services/base_api_client/base_api_client.py
+++ base/services/base_api_client/base_api_client.py
@@ -2,15 +2,23 @@ import os
 
 from abc import ABC
 from abc import abstractmethod
+from typing import Any
+from typing import Literal
 from urllib.parse import quote
 
 import requests
 
+from base.models import ClientLog
+
 DEFAULT_TIMEOUT = 10
 DEFAULT_SCHEME = "https"
 
+Method = Literal["get", "post", "patch", "put", "delete"]
+
 
 class BaseApiClient(ABC):
+    code: str | None = None
+
     def __init__(self) -> None:
         self.configuration = self.get_configuration()
 
@@ -20,11 +28,11 @@ class BaseApiClient(ABC):
         path_params: dict[str, str | int] | None = None,
         query_params: dict[str, str | int] | None = None,
     ) -> requests.Response:
-        url = self.get_url(endpoint, path_params)
-        return requests.get(
-            url,
+        return self.request(
+            "get",
+            endpoint=endpoint,
+            path_params=path_params,
             params=query_params,
-            timeout=self.configuration["timeout"],
         )
 
     def post_blocking(
@@ -32,14 +40,14 @@ class BaseApiClient(ABC):
         endpoint: str,
         path_params: dict[str, str | int] | None = None,
         query_params: dict[str, str | int] | None = None,
-        body: dict[str, str] | None = None,
+        body: dict[str, Any] | None = None,
     ) -> requests.Response:
         url = self.get_url(endpoint, path_params)
-        return requests.post(
+        return self.request(
+            "post",
             url,
             json=body,
             params=query_params,
-            timeout=self.configuration["timeout"],
         )
 
     def patch_blocking(
@@ -47,14 +55,14 @@ class BaseApiClient(ABC):
         endpoint: str,
         path_params: dict[str, str | int] | None = None,
         query_params: dict[str, str | int] | None = None,
-        body: dict[str, str] | None = None,
+        body: dict[str, Any] | None = None,
     ) -> requests.Response:
-        url = self.get_url(endpoint, path_params)
-        return requests.patch(
-            url,
+        return self.request(
+            "patch",
+            endpoint=endpoint,
+            path_params=path_params,
             json=body,
             params=query_params,
-            timeout=self.configuration["timeout"],
         )
 
     def put_blocking(
@@ -62,14 +70,14 @@ class BaseApiClient(ABC):
         endpoint: str,
         path_params: dict[str, str | int] | None = None,
         query_params: dict[str, str | int] | None = None,
-        body: dict[str, str] | None = None,
+        body: dict[str, Any] | None = None,
     ) -> requests.Response:
-        url = self.get_url(endpoint, path_params)
-        return requests.put(
-            url,
+        return self.request(
+            "put",
+            endpoint=endpoint,
+            path_params=path_params,
             json=body,
             params=query_params,
-            timeout=self.configuration["timeout"],
         )
 
     def delete_blocking(
@@ -77,10 +85,44 @@ class BaseApiClient(ABC):
         endpoint: str,
         path_params: dict[str, str | int] | None = None,
     ) -> requests.Response:
-        url = self.get_url(endpoint, path_params)
-        return requests.delete(
-            url,
-            timeout=self.configuration["timeout"],
+        return self.request(
+            "delete",
+            endpoint=endpoint,
+            path_params=path_params,
+        )
+
+    def request(
+        self,
+        method: Method,
+        endpoint: str,
+        path_params: dict[str, str | int] | None = None,
+        **kwargs,
+    ) -> requests.Response:
+        try:
+            url = self.get_url(endpoint, path_params)
+            log = self.create_log(method, endpoint, url, kwargs.get("json", ""))
+            response = self.make_request(method, url, **kwargs)
+            self.update_log(log, response)
+        except Exception as error:
+            log.request_error = str(error)
+            log.save()
+            raise Exception from error
+        else:
+            return response
+
+    def update_log(self, log, response):
+        log.response_headers = str(response.headers)
+        log.response_content = response.text
+        log.save()
+
+    def make_request(
+        self,
+        method: Method,
+        url: str,
+        **kwargs,
+    ):
+        return requests.request(
+            method, url, timeout=self.configuration["timeout"], **kwargs
         )
 
     def get_url(
@@ -98,6 +140,30 @@ class BaseApiClient(ABC):
             return parsed_endpoint.format(**parsed_path_params)
         return parsed_endpoint
 
+    def create_log(
+        self, method: Method, endpoint: str, url: str, request_body: dict | None
+    ):
+        return ClientLog.objects.create(
+            method=method,
+            url=url,
+            endpoint=endpoint,
+            client_url=self.base_url,
+            client_code=self.client_code,
+            request_content=request_body,
+        )
+
+    def get_configuration(self) -> dict:
+        return {
+            **self.get_default_configuration(),
+            **self.get_extra_configuration(),
+        }
+
+    def get_default_configuration(self):
+        return {
+            "timeout": DEFAULT_TIMEOUT,
+            "scheme": DEFAULT_SCHEME,
+        }
+
     @staticmethod
     def parse_path_params(
         path_params: dict[str, str | int] | None = None
@@ -111,22 +177,14 @@ class BaseApiClient(ABC):
         }
 
     @property
+    def client_code(self):
+        return self.code or self.__class__.__name__
+
+    @property
     def base_url(self) -> str:
         scheme = self.configuration["scheme"]
         host = self.configuration["host"]
         return f"{scheme}://{host.strip('/')}"
-
-    def get_configuration(self) -> dict:
-        return {
-            **self.get_default_configuration(),
-            **self.get_extra_configuration(),
-        }
-
-    def get_default_configuration(self):
-        return {
-            "timeout": DEFAULT_TIMEOUT,
-            "scheme": DEFAULT_SCHEME,
-        }
 
     @abstractmethod
     def get_extra_configuration(self) -> dict:
