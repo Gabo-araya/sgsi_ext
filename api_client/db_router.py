4061de5205141f5f6de07173b9c5afdd87f484bd
Raul Esteban <raul.esteban@magnet.cl>
2023-05-17 15:56:47 -0400

    fix: db_router wasn't running migrations

--- api_client/db_router.py
+++ api_client/db_router.py
@@ -4,6 +4,7 @@ from api_client.models import ClientLog
 class ClientLogDbRouter:
     # TODO: Change this value to "logs" when the automatic creation and migration of this database is implemented  # noqa: E501
     LOG_DB = "default"
+    DEFAULT_DB = "default"
 
     def db_for_read(self, model, **hints):
         if model == ClientLog:
@@ -16,6 +17,8 @@ class ClientLogDbRouter:
         return None
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
+        if self.LOG_DB == self.DEFAULT_DB:
+            return True
         if db == self.LOG_DB and model_name == "clientlog":
             return True
         return db != self.LOG_DB and model_name != "clientlog"
