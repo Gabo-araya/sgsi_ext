8fb7e5af6f4a47aa3f9b71747b67cc4de40966f6
Nicolás Stuardo Díaz <nicolas.stuardo@magnet.cl>
2023-05-26 12:41:10 -0400

    feat: send Accept header for JSON requests
    Implements: DPT-158

--- api_client/services/client/json_api_client.py
+++ api_client/services/client/json_api_client.py
@@ -4,6 +4,7 @@ import requests
 
 from .api_client import ApiClient
 from .types import JSONType
+from .types import Method
 from .types import UploadFiles
 
 
@@ -13,8 +14,11 @@ class JsonApiClient(ApiClient):
         endpoint: str,
         path_params: dict[str, str | int] | None = None,
         query_params: dict[str, str | int] | None = None,
+        headers: dict[str, Any] | None = None,
     ) -> tuple[tuple[JSONType, int], requests.RequestException | None]:
-        response, error = super().get_blocking(endpoint, path_params, query_params)
+        response, error = super().get_blocking(
+            endpoint, path_params, query_params, headers
+        )
         return (self.get_response_json(response), response.status_code), error
 
     def post_blocking(  # noqa: PLR0913
@@ -25,9 +29,10 @@ class JsonApiClient(ApiClient):
         data: dict[str, Any] | None = None,
         json: JSONType = None,
         files: UploadFiles | None = None,
+        headers: dict[str, Any] | None = None,
     ) -> tuple[tuple[JSONType, int], requests.RequestException | None]:
         response, error = super().post_blocking(
-            endpoint, path_params, query_params, data, json, files
+            endpoint, path_params, query_params, data, json, files, headers
         )
         return (self.get_response_json(response), response.status_code), error
 
@@ -39,9 +44,10 @@ class JsonApiClient(ApiClient):
         data: dict[str, Any] | None = None,
         json: JSONType = None,
         files: UploadFiles | None = None,
+        headers: dict[str, Any] | None = None,
     ) -> tuple[tuple[JSONType, int], requests.RequestException | None]:
         response, error = super().patch_blocking(
-            endpoint, path_params, query_params, data, json, files
+            endpoint, path_params, query_params, data, json, files, headers
         )
         return (self.get_response_json(response), response.status_code), error
 
@@ -53,9 +59,10 @@ class JsonApiClient(ApiClient):
         data: dict[str, Any] | None = None,
         json: JSONType = None,
         files: UploadFiles | None = None,
+        headers: dict[str, Any] | None = None,
     ) -> tuple[tuple[JSONType, int], requests.RequestException | None]:
         response, error = super().put_blocking(
-            endpoint, path_params, query_params, data, json, files
+            endpoint, path_params, query_params, data, json, files, headers
         )
         return (self.get_response_json(response), response.status_code), error
 
@@ -63,9 +70,24 @@ class JsonApiClient(ApiClient):
         self,
         endpoint: str,
         path_params: dict[str, str | int] | None = None,
+        headers: dict[str, Any] | None = None,
     ) -> tuple[tuple[JSONType, int], requests.RequestException | None]:
-        response, error = super().delete_blocking(endpoint, path_params)
+        response, error = super().delete_blocking(endpoint, path_params, headers)
         return (self.get_response_json(response), response.status_code), error
+
+    def request_blocking(
+        self,
+        method: Method,
+        endpoint: str,
+        path_params: dict[str, str | int] | None = None,
+        **kwargs,
+    ) -> tuple[requests.Response, requests.RequestException | None]:
+        kwargs.setdefault("headers", {})
+        if kwargs["headers"] is None:
+            kwargs["headers"] = {}
+        kwargs["headers"].setdefault("Accept", "application/json")
+
+        return super().request_blocking(method, endpoint, path_params, **kwargs)
 
     @staticmethod
     def get_response_json(response: requests.Response) -> JSONType:
