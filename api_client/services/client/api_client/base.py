40ba1c92a88b5b105dc895b5c593cdb4233c1fb3
Raul Esteban <raul.esteban@magnet.cl>
2023-06-02 12:35:09 -0400

    feat: validate timeout

--- api_client/services/client/api_client/base.py
+++ api_client/services/client/api_client/base.py
@@ -1,6 +1,9 @@
+from django.conf import settings
+
 from api_client.enums import ClientCodes
 
 from ..config import ApiClientConfiguration
+from ..errors import ClientConfigurationError
 
 
 class BaseApiClient:
@@ -9,11 +12,20 @@ class BaseApiClient:
         self.validate_configuration()
 
     def validate_configuration(self) -> None:
-        # Try coercing value into an enum one. If this fail then raise an error.
-        # This is done to support the non-blocking implementation which serializes
-        # the enum into a string.
+        self.validate_client_code()
+        self.validate_timeout()
+
+    def validate_client_code(self):
         try:
             ClientCodes(self.configuration.code)
         except ValueError as e:
             msg = f"Invalid client code: {self.configuration.code}"
-            raise TypeError(msg) from e
+            raise ClientConfigurationError(msg) from e
+
+    def validate_timeout(self) -> None:
+        if self.configuration.timeout > settings.DEFAULT_TIMEOUT:
+            msg = (
+                f"Can't set timeout at {self.configuration.timeout}s, the max is "
+                f"{settings.DEFAULT_TIMEOUT}s"
+            )
+            raise ClientConfigurationError(msg)
