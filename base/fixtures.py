78f27e13ba305527fe0320e9a0cb23723f214721
Nicolás Stuardo Díaz <nicolas.stuardo@magnet.cl>
2023-06-23 16:31:32 -0400

    fix: add finalizer to client fixtures that clears RequestMiddleware locals
    Thread locals are used to expose the currently logged user so audit_log can retrieve the user to create log objects.
    Client fixtures not only return a Client object but also create a user and logs it in, meaning thread locals were being set without being cleaned after test ended. This caused tests not requiring those fixtures to fail as audit_log was attempting to use a user that not longer exists.

--- base/fixtures.py
+++ base/fixtures.py
@@ -2,35 +2,43 @@ from typing import TYPE_CHECKING
 
 import pytest
 
+from base.middleware import RequestMiddleware
+
 if TYPE_CHECKING:
-    import django.test.client
+    from django.test.client import Client
 
 
 @pytest.fixture
-def superuser_client(db, superuser_user) -> "django.test.client.Client":
+def superuser_client(db, superuser_user) -> "Client":
     """A Django test client logged in as an admin user."""
     from django.test.client import Client
 
     client = Client()
     client.force_login(superuser_user)
-    return client
+    yield client
+    if hasattr(RequestMiddleware.thread_local, "user"):
+        del RequestMiddleware.thread_local.user
 
 
 @pytest.fixture
-def staff_client(db, staff_user) -> "django.test.client.Client":
+def staff_client(db, staff_user) -> "Client":
     """A Django test client logged in as an admin user."""
     from django.test.client import Client
 
     client = Client()
     client.force_login(staff_user)
-    return client
+    yield client
+    if hasattr(RequestMiddleware.thread_local, "user"):
+        del RequestMiddleware.thread_local.user
 
 
 @pytest.fixture
-def regular_user_client(db, regular_user) -> "django.test.client.Client":
+def regular_user_client(db, regular_user) -> "Client":
     """A Django test client logged in as an admin user."""
     from django.test.client import Client
 
     client = Client()
     client.force_login(regular_user)
-    return client
+    yield client
+    if hasattr(RequestMiddleware.thread_local, "user"):
+        del RequestMiddleware.thread_local.user
