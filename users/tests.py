9c7572a9ee6b9845d1cdf15974d73da7ba891b42
Nicolás Stuardo Díaz <nicolas.stuardo@magnet.cl>
2023-06-23 18:10:58 -0400

    test: add UserCreationForm cleaning tests

--- users/tests.py
+++ users/tests.py
@@ -7,10 +7,17 @@ from urllib.parse import urlparse
 
 from django.urls import reverse
 
-# tests
-from base.tests import BaseTestCase
+import pytest
+
 from users.forms import UserCreationForm
 from users.models import User
+
+
+@pytest.fixture
+def duplicated_user(db):
+    return User.objects.create_user(
+        first_name="Alex", last_name="Smith", email="dupetest@example.com"
+    )
 
 
 def test_lower_case_emails(regular_user):
@@ -44,46 +51,43 @@ def test_force_logout(regular_user_client, regular_user):
     assert parsed_location.path == login_url
 
 
-class UserCreationFormTests(BaseTestCase):
-    def test_user_creation_form_cleaning(self):
-        input_data = {
-            "email": "test@example.com",
-            "first_name": "Alex",
-            "last_name": "Schmidt",
-            "password1": "verysecretpleasedontcopy(c)magnet",
-            "password2": "verysecretpleasedontcopy(c)magnet",
-        }
-        form = UserCreationForm(data=input_data)
-        form.full_clean()
-        self.assertFalse(form.errors)
+def test_user_creation_form_cleaning(db):
+    input_data = {
+        "email": "test@example.com",
+        "first_name": "Alex",
+        "last_name": "Schmidt",
+        "password1": "verysecretpleasedontcopy(c)magnet",
+        "password2": "verysecretpleasedontcopy(c)magnet",
+    }
+    form = UserCreationForm(data=input_data)
+    form.full_clean()
+    assert not form.errors
 
-    def test_user_creation_form_clean_duplicated_user(self):
-        User.objects.create_user(
-            first_name="Alex", last_name="Smith", email="dupetest@example.com"
-        )
 
-        input_data = {
-            "email": "dupetest@example.com",
-            "first_name": "Alex",
-            "last_name": "Schmidt",
-            "password1": "verysecretpleasedontcopy(c)magnet",
-            "password2": "verysecretpleasedontcopy(c)magnet",
-        }
-        form = UserCreationForm(data=input_data)
-        form.full_clean()
-        self.assertIn("email", form.errors)
+def test_user_creation_form_clean_duplicated_user(duplicated_user):
+    input_data = {
+        "email": "dupetest@example.com",
+        "first_name": "Alex",
+        "last_name": "Schmidt",
+        "password1": "verysecretpleasedontcopy(c)magnet",
+        "password2": "verysecretpleasedontcopy(c)magnet",
+    }
+    form = UserCreationForm(data=input_data)
+    form.full_clean()
+    assert "email" in form.errors
 
-    def test_user_creation_form_clean_password_mismatch(self):
-        input_data = {
-            "email": "test@example.com",
-            "first_name": "Alex",
-            "last_name": "Schmidt",
-            "password1": "verysecretpleasedontcopy(c)magnet",
-            "password2": "verysecretpleasedontsteal(c)magnet",
-        }
-        form = UserCreationForm(data=input_data)
-        form.full_clean()
-        errors = form.errors
-        self.assertIn("password2", form.errors)
-        password_error = errors["password2"]
-        self.assertEqual(password_error.data[0].code, "password_mismatch")
+
+def test_user_creation_form_clean_password_mismatch(db):
+    input_data = {
+        "email": "test@example.com",
+        "first_name": "Alex",
+        "last_name": "Schmidt",
+        "password1": "verysecretpleasedontcopy(c)magnet",
+        "password2": "verysecretpleasedontsteal(c)magnet",
+    }
+    form = UserCreationForm(data=input_data)
+    form.full_clean()
+    errors = form.errors
+    assert "password2" in errors
+    password_error = errors["password2"]
+    assert password_error.data[0].code == "password_mismatch"
