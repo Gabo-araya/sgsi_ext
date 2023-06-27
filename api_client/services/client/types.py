656ad37ee9eda2549bfcdea10050259dfaf64666
Nicolás Stuardo Díaz <nicolas.stuardo@magnet.cl>
2023-06-27 10:15:20 -0400

    style: remove empty line in types.py

--- api_client/services/client/types.py
+++ api_client/services/client/types.py
@@ -9,5 +9,4 @@ Method = Literal["get", "post", "patch", "put", "delete"]
 Readable = IO | str | bytes | bytearray
 UploadFiles = dict[str, Readable] | Sequence[tuple[str, Readable]]
 JSONType = str | int | float | bool | None | dict[str, "JSONType"] | list["JSONType"]
-
 Callback = Callable[[requests.Response, requests.RequestException | None], None]
