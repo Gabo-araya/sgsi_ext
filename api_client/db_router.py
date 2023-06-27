200dac96c191849f4134d358c71d615d08668e81
Nicolás Stuardo Díaz <nicolas.stuardo@magnet.cl>
2023-06-27 09:36:14 -0400

    fix: remove LOG_DB == DEFAULT_DB check

--- api_client/db_router.py
+++ api_client/db_router.py
@@ -3,7 +3,6 @@ from api_client.models import ClientLog
 
 class ClientLogDbRouter:
     LOG_DB = "logs"
-    DEFAULT_DB = "default"
 
     def db_for_read(self, model, **hints):
         if model == ClientLog:
@@ -16,8 +15,6 @@ class ClientLogDbRouter:
         return None
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
-        if self.LOG_DB == self.DEFAULT_DB:
-            return True
         if db == self.LOG_DB and model_name == "clientlog":
             return True
         return db != self.LOG_DB and model_name != "clientlog"
