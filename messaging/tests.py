0f564c239029424c59ff1c09af88c702875c5fb4
Nicolás Stuardo Díaz <nicolas.stuardo@magnet.cl>
2023-06-29 17:53:14 -0400

    test: add email manager tests

--- messaging/tests.py
+++ messaging/tests.py
@@ -1,3 +1,35 @@
-from django.test import TestCase  # noqa: F401
+from django.core import mail
 
-# Create your tests here.
+from base.tests import BaseTestCase
+from messaging.email_manager import send_emails
+
+
+class EmailManagerTests(BaseTestCase):
+    def test_send_email(self):
+        with self.settings(TEST=False):
+            context = {
+                "value_a": 123,
+                "value_b": "test",
+                "value_c": True,
+            }
+
+            send_emails(
+                ["test@example.com"],
+                "test_email",
+                "Testing email",
+                "mailer@example.com",
+                context,
+            )
+
+            self.assertGreater(len(mail.outbox), 0)
+
+    def test_send_email_no_context(self):
+        with self.settings(TEST=False):
+            send_emails(
+                ["test@example.com"],
+                "test_email",
+                "Testing email",
+                "mailer@example.com",
+            )
+
+            self.assertGreater(len(mail.outbox), 0)
