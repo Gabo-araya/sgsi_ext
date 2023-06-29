efd9be773f7491a47f810ed5d57ebc5782ed7fde
Nicolás Stuardo Díaz <nicolas.stuardo@magnet.cl>
2023-06-29 17:49:17 -0400

    test: add regions tests

--- regions/tests.py
+++ regions/tests.py
@@ -1,17 +1,63 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
+from http import HTTPStatus
+from urllib.parse import urlencode
 
-Replace this with more appropriate tests for your application.
-"""
+from django.db.models import CharField
+from django.db.models import F
+from django.db.models import Value
+from django.db.models.functions import Cast
+from django.db.models.functions import Concat
+from django.urls import reverse
+
+from base.tests import BaseTestCase
+from regions.models import Commune
+from regions.models import Region
 
 
-from django.test import TestCase
+class RegionViewsTests(BaseTestCase):
+    def test_search_communes_by_name(self):
+        # given
+        query_data = {"commune": "Osorno"}
 
+        # when
+        url = reverse("search_communes")
+        response = self.client.get(url + f"?{urlencode(query_data)}")
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(2, 1 + 1)
+        # then
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        parsed_content = response.json()
+
+        expected_communes = list(
+            Commune.objects.annotate(
+                text=Concat(Cast("id", CharField()), Value(" - "), F("name"))
+            )
+            .filter(name="Osorno")
+            .values("id", "text")
+            .order_by("id")
+        )
+
+        self.assertEqual(parsed_content, expected_communes)
+
+    def test_search_communes_by_id(self):
+        region_ten = Region.objects.get(order=10)
+
+        # given
+        query_data = {"regionId": region_ten.pk}
+
+        # when
+        url = reverse("search_communes")
+        response = self.client.get(url + f"?{urlencode(query_data)}")
+
+        # then
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        parsed_content = response.json()
+
+        expected_communes = list(
+            Commune.objects.annotate(
+                text=Concat(Cast("id", CharField()), Value(" - "), F("name"))
+            )
+            .filter(region_id=region_ten.pk)
+            .values("id", "text")
+            .order_by("id")
+        )
+
+        self.assertEqual(parsed_content, expected_communes)
