6f9fa5fb9d9d6477ff770ac7c482e6dd47fb4201
Nicolás Stuardo Díaz <nicolas.stuardo@magnet.cl>
2023-07-04 11:22:47 -0400

    test: add tests for api client configuration serialization and callback validation

--- api_client/tests/api_client_tests.py
+++ api_client/tests/api_client_tests.py
@@ -2,7 +2,9 @@ from unittest.mock import patch
 
 import pytest
 
-from ..services.client.errors import DisabledClientError
+from api_client.services.client import config
+from api_client.services.client import errors
+from api_client.services.client.auth import BasicAuth
 
 PARAMETRIZED_GET_URL_TESTS = {
     "empty": ("", None, "http://example.com/"),
@@ -79,4 +81,52 @@ def test_making_request_to_disabled_client_should_return_error(
         response, error = disabled_apiclient.get_blocking("/test")
 
     assert response == disabled_apiclient.empty_response
-    assert isinstance(error, DisabledClientError)
+    assert isinstance(error, errors.DisabledClientError)
+
+
+def test_serialize_configuration():
+    with patch("api_client.services.client.ApiClient.validate_configuration"):
+        auth = BasicAuth("magnet", "verysecretpleasedontsteal")
+        config_obj = config.ApiClientConfiguration(
+            scheme="http", host="example.com", code="test", auth=auth
+        )
+
+    expected_serialization = {
+        "host": "example.com",
+        "code": "test",
+        "scheme": "http",
+        "timeout": config.DEFAULT_TIMEOUT,
+        "auth_class": "api_client.services.client.auth.BasicAuth",
+        "auth_class_config": {
+            "username": "magnet",
+            "password": "verysecretpleasedontsteal",
+        },
+    }
+    assert config_obj.serialize() == expected_serialization
+
+
+def test_deserialize_configuration():
+    serialized_config = {
+        "code": "test",
+        "scheme": "http",
+        "host": "example.com",
+        "timeout": config.DEFAULT_TIMEOUT,
+        "auth_class": "api_client.services.client.auth.BasicAuth",
+        "auth_class_config": {
+            "username": "magnet",
+            "password": "verysecretpleasedontsteal",
+        },
+    }
+
+    with patch("api_client.services.client.ApiClient.validate_configuration"):
+        config_obj = config.ApiClientConfiguration.from_serialized_configuration(
+            **serialized_config
+        )
+
+    assert config_obj.code == "test"
+    assert config_obj.scheme == "http"
+    assert config_obj.host == "example.com"
+    assert config_obj.timeout == config.DEFAULT_TIMEOUT
+    assert isinstance(config_obj.auth, BasicAuth)
+    assert config_obj.auth.username == "magnet"
+    assert config_obj.auth.password == "verysecretpleasedontsteal"  # noqa: S105
