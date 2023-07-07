ee904dc01d2785cf9825455b6712c26c26470f94
Nicolás Stuardo Díaz <nicolas.stuardo@magnet.cl>
2023-07-07 10:00:48 -0400

    test: add test for valid, formatted and unformatted RUT

--- base/tests/test_fields.py
+++ base/tests/test_fields.py
@@ -16,6 +16,8 @@ from base.fields import ChileanRUTField
         ("103-1", "103-1", does_not_raise()),
         ("1105-3", "1.105-3", does_not_raise()),
         ("444444444", "44.444.444-4", does_not_raise()),
+        ("48395858-7", "48.395.858-7", does_not_raise()),
+        ("69.290.603-9", "69.290.603-9", does_not_raise()),
         ("44444444-K", None, pytest.raises(ValidationError)),
         ("invalidrut", None, pytest.raises(ValidationError)),
     ),
