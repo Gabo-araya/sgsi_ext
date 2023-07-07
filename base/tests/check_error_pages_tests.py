aff71f7b2e84b80b7bed0f247bb8ddd263289ca9
Nicolás Stuardo Díaz <nicolas.stuardo@magnet.cl>
2023-07-07 09:37:33 -0400

    chore: remove TestCase leftovers

--- base/tests/check_error_pages_tests.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-
-
-class CheckErrorPagesTest(TestCase):
-    def test_404(self):
-        response = self.client.get("/this-url-does-not-exist")
-        self.assertTemplateUsed(response, "exceptions/404.pug")
--- base/tests/test_utils.py
+++ base/tests/test_utils.py
@@ -1,10 +1,11 @@
 import pytest
 
 from base.utils import format_rut
+from base.utils import validate_rut
 
 
 @pytest.mark.parametrize(
-    "rut,expected_value",
+    ("raw_value", "expected_value"),
     (
         ("", ""),
         ("invalidrut", ""),
@@ -17,5 +18,27 @@ from base.utils import format_rut
         ("444444444", "44.444.444-4"),
     ),
 )
-def test_format_rut(rut, expected_value):
-    assert format_rut(rut) == expected_value
+def test_format_rut(raw_value, expected_value):
+    assert format_rut(raw_value) == expected_value
+
+
+@pytest.mark.parametrize(
+    "raw_value, expected_result",
+    (
+        ("1-9", True),
+        ("27-2", True),
+        ("27-1", False),
+        ("103-1", True),
+        ("103-K", False),
+        ("1105-3", True),
+        ("1105-0", False),
+        ("12350-1", True),
+        ("12350-5", False),
+        ("654278-6", True),
+        ("654278-1", False),
+        ("44444444-4", True),
+        ("44444444-K", False),
+    ),
+)
+def test_validate_rut(raw_value, expected_result):
+    assert validate_rut(raw_value) == expected_result
