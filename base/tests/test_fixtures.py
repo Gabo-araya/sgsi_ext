475080b20d4872eb3dc8b0afac2815fe6a013f20
Nicolás Stuardo Díaz <nicolas.stuardo@magnet.cl>
2023-07-11 17:10:13 -0400

    feat: check returned fixture is a model instance

--- base/tests/test_fixtures.py
+++ base/tests/test_fixtures.py
@@ -9,22 +9,18 @@ from base.utils import get_our_models
 @pytest.mark.django_db(databases=["default", "logs"])
 def test_all_models_have_fixtures(request):
     """Checks all our models have at least a test fixture defined."""
-
     models_without_fixtures = []
 
     for model_class in get_our_models():
-        model_name = underscore(model_class.__name__)
-        django_name = f"{model_class._meta.app_label}.{model_class.__name__}"
-        try:
-            # Try with custom name first
-            fixture_name = MODEL_FIXTURE_CUSTOM_NAMES.get(django_name)
-            request.getfixturevalue(fixture_name)
-        except pytest.FixtureLookupError:
-            try:
-                # Try with default name
-                request.getfixturevalue(model_name)
-            except pytest.FixtureLookupError:
-                models_without_fixtures.append((model_class, model_name))
+        fixture_name = get_fixture_name_for_model(model_class)
+        fixture = request.getfixturevalue(fixture_name)
+        if not fixture:
+            models_without_fixtures.append((model_class, fixture_name))
+            continue
+        assert isinstance(fixture, model_class), (
+            f'Fixture "{fixture_name}" did not return'
+            f" an {model_class.__name__} instance!"
+        )
 
     assert not models_without_fixtures, (
         "Found model(s) without defined test fixture(s): \n"
@@ -36,3 +32,12 @@ def test_all_models_have_fixtures(request):
         + "\nEnsure fixtures with the above mentioned names exist and are "
         "available globally."
     )
+
+
+def get_fixture_name_for_model(model_class):
+    try:
+        model_name = underscore(model_class.__name__)
+        django_name = f"{model_class._meta.app_label}.{model_class.__name__}"
+        return MODEL_FIXTURE_CUSTOM_NAMES.get(django_name, model_name)
+    except pytest.FixtureLookupError:
+        return None
