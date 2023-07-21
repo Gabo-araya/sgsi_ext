f66e70287eb4726ff6da0ee1ec8b4b33d778a5d5
Tomás Andrighetti <tomas.andrighetti@magnet.cl>
2023-07-21 18:52:45 -0400

    fix: change infiltrated compose-v1 commands

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 GIT_REF=$(git describe --tags --always --abbrev --dirty)
 BUILD_TIME=$(date --iso-8601=seconds)
 
-docker-compose build --build-arg GIT_REF="$GIT_REF" --build-arg BUILD_TIME="$BUILD_TIME"
+docker compose build --build-arg GIT_REF="$GIT_REF" --build-arg BUILD_TIME="$BUILD_TIME"
