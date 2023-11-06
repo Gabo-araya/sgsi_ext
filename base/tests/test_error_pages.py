a4c71f57894dc2151312cdc7f87350cfcbc15f6b
Joao S. López <joao@magnet.cl>
2023-11-06 10:46:12 -0300

    chore: migrate base templates from pug to html

--- base/tests/test_error_pages.py
+++ base/tests/test_error_pages.py
@@ -3,4 +3,4 @@ from pytest_django.asserts import assertTemplateUsed
 
 def test_404(client):
     response = client.get("/this-url-does-not-exist")
-    assertTemplateUsed(response, "exceptions/404.pug")
+    assertTemplateUsed(response, "exceptions/404.html")
