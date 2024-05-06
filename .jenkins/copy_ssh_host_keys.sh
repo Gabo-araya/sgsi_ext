37222f32cf076bbb638afc98a666b76f4c1aafe4
Nicolás Stuardo Díaz <nicolas.stuardo@magnet.cl>
2024-05-06 18:09:15 -0400

    fix: use upgraded copy_ssh_host_keys.sh script

--- .jenkins/copy_ssh_host_keys.sh
+++ .jenkins/copy_ssh_host_keys.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
-set -euo pipefail
+set -euxo pipefail
 
-if (( $# == 0 )); then
-  echo "Usage: $0 <inventory_entry>"
-  exit 1
-fi
-target=$1
-
-host_ssh_key=$(yq --raw-output ".all.hosts.\"$target\".ansible_host + \" \" + .all.hosts.\"$target\".host_ssh_key" ansible/inventory.yml)
-host_ssh_key="${host_ssh_key%%*[[:space:]]}"
-
-if [[ -z "$host_ssh_key" ]]; then
-  echo "No host keys found for target $target"
-  exit 1
-fi
-
-echo "Adding host key for $target"
-echo "$host_ssh_key" >> ~/.ssh/known_hosts
+for target in $(yq --raw-output ".all.hosts | to_entries[] | select(.value.branch == \"$BRANCH_NAME\") | .key" ansible/inventory.yml); do
+  host_ssh_key=$(yq --raw-output ".all.hosts.\"$target\".ansible_host + \" \" + .all.hosts.\"$target\".host_ssh_key" ansible/inventory.yml)
+  echo "Adding host key for $target"
+  echo "$host_ssh_key" >> ~/.ssh/known_hosts
+done
